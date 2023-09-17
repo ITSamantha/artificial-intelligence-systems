f34dbaf2162131d26d55d0b0e2274a6fd15c4e8e
ITSamantha <you@example.com>
2023-09-17 13:11:49 +0300

    Lab1 modified.

--- module1/lab1/lab1.pl
+++ module1/lab1/lab1.pl
@@ -9,6 +9,20 @@ videogame('Overwatch').
 videogame('Super Mario Bros').
 videogame('League of Legends').
 videogame('The Sims 3').
+videogame('Red Dead Redemption 2').
+videogame('The Legend of Zelda: Breath of the Wild').
+videogame('Fortnite').
+videogame('Call of Duty: Warzone').
+videogame('Dark Souls III').
+videogame('Grand Theft Auto V').
+videogame('FIFA 22').
+videogame('Cyberpunk 2077').
+videogame('Resident Evil Village').
+videogame('The Elder Scrolls V: Skyrim').
+videogame('Halo Infinite').
+videogame('Among Us').
+videogame('Mortal Kombat 11').
+videogame('Assassin\'s Creed Valhalla').
 
 % Жанры видеоигр.
 genre('The Witcher 3: Wild Hunt', 'RPG').
@@ -17,6 +31,20 @@ genre('Overwatch', 'Shooter').
 genre('Super Mario Bros', 'Platformer').
 genre('League of Legends', 'Moba').
 genre('The Sims 3', 'Simulator').
+genre('Red Dead Redemption 2', 'Action-Adventure').
+genre('The Legend of Zelda: Breath of the Wild', 'Action-Adventure').
+genre('Fortnite', 'Battle Royale').
+genre('Call of Duty: Warzone', 'Battle Royale').
+genre('Dark Souls III', 'Action RPG').
+genre('Grand Theft Auto V', 'Action-Adventure').
+genre('FIFA 22', 'Sports').
+genre('Cyberpunk 2077', 'Action RPG').
+genre('Resident Evil Village', 'Survival Horror').
+genre('The Elder Scrolls V: Skyrim', 'Action RPG').
+genre('Halo Infinite', 'First-Person Shooter').
+genre('Among Us', 'Social Deduction').
+genre('Mortal Kombat 11', 'Fighting').
+genre('Assassin\'s Creed Valhalla', 'Action-Adventure').
 
 % Главный персонаж.
 main_character('The Witcher 3: Wild Hunt', 'Geralt of Rivia').
@@ -25,6 +53,20 @@ main_character('Overwatch', 'Tracer').
 main_character('Super Mario Bros', 'Mario').
 main_character('League of Legends', 'Summoner').
 main_character('The Sims 3', 'Empty').
+main_character('Red Dead Redemption 2', 'Arthur Morgan').
+main_character('The Legend of Zelda: Breath of the Wild', 'Link').
+main_character('Fortnite', 'Default Skin').
+main_character('Call of Duty: Warzone', 'Custom Operator').
+main_character('Dark Souls III', 'The Ashen One').
+main_character('Grand Theft Auto V', 'Michael De Santa').
+main_character('FIFA 22', 'Custom Player').
+main_character('Cyberpunk 2077', 'V').
+main_character('Resident Evil Village', 'Ethan Winters').
+main_character('The Elder Scrolls V: Skyrim', 'Dragonborn').
+main_character('Halo Infinite', 'Master Chief').
+main_character('Among Us', 'Crewmember').
+main_character('Mortal Kombat 11', 'Various Fighters').
+main_character('Assassin\'s Creed Valhalla', 'Eivor').
 
 % Рейтинг.
 rating('The Witcher 3: Wild Hunt', 9.3).
@@ -33,6 +75,20 @@ rating('Overwatch', 7.9).
 rating('Super Mario Bros', 8.1).
 rating('League of Legends', 8.7).
 rating('The Sims 3', 10).
+rating('Red Dead Redemption 2', 9.7).
+rating('The Legend of Zelda: Breath of the Wild', 9.5).
+rating('Fortnite', 7.2).
+rating('Call of Duty: Warzone', 7.8).
+rating('Dark Souls III', 8.9).
+rating('Grand Theft Auto V', 9.6).
+rating('FIFA 22', 7.9).
+rating('Cyberpunk 2077', 6.7).
+rating('Resident Evil Village', 8.4).
+rating('The Elder Scrolls V: Skyrim', 9.3).
+rating('Halo Infinite', 8.5).
+rating('Among Us', 8.4).
+rating('Mortal Kombat 11', 8.8).
+rating('Assassin\'s Creed Valhalla', 8.2).
 
 % Механика и тип.
 mechanic('The Witcher 3: Wild Hunt', 'Open world', 'Single player').
@@ -41,6 +97,20 @@ mechanic('Overwatch', 'Team', 'Multiplayer').
 mechanic('Super Mario Bros', 'Side Scrolling', 'Single player').
 mechanic('League of Legends', 'Battle Arena', 'Multiplayer').
 mechanic('The Sims 3', 'Open World', 'Single player').
+mechanic('Red Dead Redemption 2', 'Open world', 'Single player').
+mechanic('The Legend of Zelda: Breath of the Wild', 'Open world', 'Single player').
+mechanic('Fortnite', 'Battle Royale', 'Multiplayer').
+mechanic('Call of Duty: Warzone', 'Battle Royale', 'Multiplayer').
+mechanic('Dark Souls III', 'Action RPG', 'Single player').
+mechanic('Grand Theft Auto V', 'Open world', 'Single player').
+mechanic('FIFA 22', 'Sports', 'Multiplayer').
+mechanic('Cyberpunk 2077', 'Action RPG', 'Single player').
+mechanic('Resident Evil Village', 'Survival Horror', 'Single player').
+mechanic('The Elder Scrolls V: Skyrim', 'Action RPG', 'Single player').
+mechanic('Halo Infinite', 'First-Person Shooter', 'Multiplayer').
+mechanic('Among Us', 'Social Deduction', 'Multiplayer').
+mechanic('Mortal Kombat 11', 'Fighting', 'Multiplayer').
+mechanic('Assassin\'s Creed Valhalla', 'Action-Adventure', 'Single player').
 
 % Факты о годе выпуска видеоигр.
 release_year('The Witcher 3: Wild Hunt', 2015).
@@ -49,6 +119,20 @@ release_year('Overwatch', 2016).
 release_year('Super Mario Bros', 1985).
 release_year('League of Legends', 2009).
 release_year('The Sims 3', 2009).
+release_year('Red Dead Redemption 2', 2018).
+release_year('The Legend of Zelda: Breath of the Wild', 2017).
+release_year('Fortnite', 2017).
+release_year('Call of Duty: Warzone', 2020).
+release_year('Dark Souls III', 2016).
+release_year('Grand Theft Auto V', 2013).
+release_year('FIFA 22', 2021).
+release_year('Cyberpunk 2077', 2020).
+release_year('Resident Evil Village', 2021).
+release_year('The Elder Scrolls V: Skyrim', 2011).
+release_year('Halo Infinite', 2021).
+release_year('Among Us', 2018).
+release_year('Mortal Kombat 11', 2019).
+release_year('Assassin\'s Creed Valhalla', 2020).
 
 % Возраст, с которого можно играть
 age('The Witcher 3: Wild Hunt', 18).
@@ -57,6 +141,20 @@ age('Overwatch', 16).
 age('Super Mario Bros', 5).
 age('League of Legends', 16).
 age('The Sims 3', 16).
+age('Red Dead Redemption 2', 18).
+age('The Legend of Zelda: Breath of the Wild', 12).
+age('Fortnite', 12).
+age('Call of Duty: Warzone', 18).
+age('Dark Souls III', 16).
+age('Grand Theft Auto V', 18).
+age('FIFA 22', 3).
+age('Cyberpunk 2077', 18).
+age('Resident Evil Village', 18).
+age('The Elder Scrolls V: Skyrim', 17).
+age('Halo Infinite', 17).
+age('Among Us', 10).
+age('Mortal Kombat 11', 17).
+age('Assassin\'s Creed Valhalla', 18).
 
 % Правило 1. Игра c высокой оценкой, если её рейтинг выше 8.5.
 well_rated(X) :-
@@ -81,3 +179,15 @@ supports_game_mode(X, Y) :-
 % Правило 6. Получить список всех высоко оцененных игр
 list_of_well_rated_games(WellRatedGames) :-
    findall(Game, well_rated(Game), WellRatedGames).
+
+% Правило 7. Получить список игр определенного жанра.
+games_of_genre(Genre, Games) :-
+   findall(Game, (genre(Game, Genre)), Games).
+
+% Правило 8. Список игр, рекомендуемых для взрослых (18+).
+games_for_adults(Games) :-
+   findall(Game, (videogame(Game), age(Game, Age), Age >= 18), Games).
+
+% Правило 9. Список игр, которые считаются "классикой". "Классические игры" - это те, которые были выпущены более 20 лет назад и имеют высокий рейтинг.
+classic_games(Games) :-
+   findall(Game, (videogame(Game), release_year(Game, Year), Year =< 2003, well_rated(Game)), Games).
